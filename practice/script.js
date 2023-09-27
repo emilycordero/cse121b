ba9e2bbdbc3b347183cf294d903a909ceb31fb7f
Emily Cordero <cordeemi031@gmail.com>
2023-09-26 18:15:58 -0700

    added final

--- practice/script.js
+++ practice/script.js
@@ -1,3 +1,6 @@
+let scores = [56, 78, 95, 77, 65, 86];
+let target = scores.indexOf(87);
+console.log(target)
 function createParagraph() {
     const para = document.createElement("p");
     para.textContent = "You clicked the button!";
